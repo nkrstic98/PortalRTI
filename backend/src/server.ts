c3bf341c5c9f6e2a3fe705105a07bb0bfbc476f9
nkrstic98 <nikolakrle998@gmail.com>
2021-01-04 15:28:22 +0100

    Otvorena konekcija sa Mongo bazom

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,23 @@
+import bodyParser from 'body-parser';
+import cors from 'cors';
 import express from 'express';
+import mongoose from 'mongoose';
+
 const app = express();
-app.get('/', (req, res) => res.send('Hello World!'));
+
+app.use(cors());
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb://localhost:27017/rti_database');
+
+const conn = mongoose.connection;
+
+conn.once('open', () => {
+  console.log('Mongo connection open');
+});
+
+const router = express.Router();
+
+app.use('/', router);
+
 app.listen(4000, () => console.log(`Express server running on port 4000`));
