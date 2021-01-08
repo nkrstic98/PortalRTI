8e3a3cbc8412b2dada696002ecfd2b5dcd4bda29
nkrstic98 <nikolakrle998@gmail.com>
2021-01-08 15:53:06 +0100

    workers.service.ts iz backend-a preimenovan u worker.service.ts

---
+++ backend/src/services/worker.service.ts
@@ -0,0 +1,74 @@
+import express from 'express';
+import User from '../model/user';
+import Worker from '../model/worker';
+
+const router = express.Router();
+
+router.route('/').get((req, res, next) => {
+  Worker.find()
+    .then(users => res.json(users))
+    .catch(err => next(err));
+})
+
+router.route('/filter/:data').get((req, res, next) => {
+  let criteria;
+
+  let data = JSON.parse(req.params.data);
+
+  if(data.active == true) {
+    if(data.type != null) {
+      criteria = {
+        title: data.type,
+        status: 'Aktivan'
+      }
+    }
+    else {
+      criteria = {
+        status: 'Aktivan'
+      }
+    }
+  }
+  else {
+    if(data.type != null) {
+      criteria = {
+        title: data.type
+      }
+    }
+    else {
+      criteria = {
+      }
+    }
+  }
+
+  Worker.find(criteria)
+    .then(users => res.json(users))
+    .catch((err => next(err)));
+})
+
+router.route('/delete').post((req, res, next) => {
+  Worker.deleteOne({username:req.body.username})
+    .then(() => {
+      User.deleteOne({username:req.body.username})
+        .then(() => res.json({}))
+        .catch((err => next(err)));
+    })
+    .catch((err => next(err)));
+})
+
+router.route('/:username').get((req, res, next) => {
+  Worker.findOne({username: req.params.username})
+    .then(user => res.json(user))
+    .catch(err => next(err));
+})
+
+router.route('/update').post((req, res, next) => {
+  Worker.findOneAndUpdate({username:req.body.username}, req.body)
+    .then(() => {
+      User.findOneAndUpdate({username: req.body.username}, {password: req.body.password, default_pass: true})
+        .then(() => res.json({}))
+        .catch(err => next(err));
+    })
+    .catch(err => next(err));
+})
+
+module.exports = router;
