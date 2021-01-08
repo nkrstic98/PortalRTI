49667002374bdea17ecb88bf00ac2cd5e299da5c
nkrstic98 <nikolakrle998@gmail.com>
2021-01-08 16:03:25 +0100

    Dodate komponente za registraciju i uredjivanje zaposlenih

---
+++ src/app/account/register-worker/register-worker.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register-worker',
+  templateUrl: './register-worker.component.html',
+  styleUrls: ['./register-worker.component.css']
+})
+export class RegisterWorkerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
